ee71d539fd912c3203738a0773d1180e0342c972
ssc255 <ssc255@cornell.edu>
2019-08-26 15:05:46 -0700

    more fancy emails

--- email/mail.py
+++ email/mail.py
@@ -1,24 +1,44 @@
 import smtplib
 import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-smtp_server = "smtp.gmail.com"
-port = 587  # For starttls
 sender_email = "cuswiperight@gmail.com"
+receiver_email = "sabhyachhabria@gmail.com"
 password = input("Type your password and press enter: ")
 
-# Create a secure SSL context
-context = ssl.create_default_context()
 
-# Try to log in to server and send email
-try:
-    server = smtplib.SMTP(smtp_server, port)
-    # server.ehlo()  # Can be omitted
-    server.starttls(context=context)  # Secure the connection
-    # server.ehlo()  # Can be omitted
+message = MIMEMultipart("alternative")
+message["Subject"] = "multipart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+# Create the HTML version of your message
+html = """\
+<html>
+  <body>
+    <p>Hi Sabhya, <br>
+      How are you? <br>
+      Your favorite dish PASTA! is available at Cook House Dining Hall
+    </p>
+    <p>Find out more here!<br>
+       <a href="https://now.dining.cornell.edu/eateries">Cornell Dining</a>
+    </p>
+  </body>
+</html>
+"""
+
+# Turn these into plain/html MIMEText objects
+main = MIMEText(html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(main)
+
+# Create secure connection with server and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
-    # TODO: Send email here
-except Exception as e:
-    # Print any error messages to stdout
-    print(e)
-finally:
-    server.quit()
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
